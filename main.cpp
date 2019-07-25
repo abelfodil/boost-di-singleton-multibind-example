4fa3e4267747a445131a6133f39329a4b49399fa
Anes Belfodil <ans.belfodil@gmail.com>
2019-07-25 17:49:16 -0400

    Add example

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <boost/di.hpp>
+
+class OperationBase {};
+
+class Operation1 : public OperationBase {
+public:
+  Operation1() { std::cout << "Operation1 constructed." << std::endl; }
+  ~Operation1() { std::cout << "Operation1 destructed." << std::endl; }
+};
+
+class Operation2 : public OperationBase {
+public:
+  Operation2() { std::cout << "Operation2 constructed." << std::endl; }
+  ~Operation2() { std::cout << "Operation2 destructed." << std::endl; }
+};
+
+class User1 {
+public:
+  explicit User1(std::vector<std::shared_ptr<OperationBase>>) {
+    std::cout << "User1 constructed." << std::endl;
+  }
+  ~User1() { std::cout << "User1 destructed." << std::endl; }
+};
+
+class User2 {
+public:
+  explicit User2(std::vector<std::shared_ptr<OperationBase>>) {
+    std::cout << "User2 constructed." << std::endl;
+  }
+  ~User2() { std::cout << "User2 destructed." << std::endl; }
+};
+
+int main() {
+  using namespace boost;
+  auto injector = di::make_injector(
+      di::bind<OperationBase *[]>().to<Operation1, Operation2>());
+
+  auto user1 = injector.create<User1 &>();
+  auto user2 = injector.create<User2 &>();
+
+  return 0;
+}
